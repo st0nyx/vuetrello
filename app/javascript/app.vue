49e06d598c7c9631c6488a75983890292de5e97a
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-04-29 16:49:40 +0200

    added vuex

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,13 @@
 <template>
   <draggable v-model="lists" :options="{group: 'lists'}" class="board dragArea" @end="listMoved">
     <list v-for="(list, index) in lists" :list="list"></list>
+
+    <div class="list">
+      <a v-if="!editing" v-on:click="startEditing">Add a List</a>
+      <textarea v-if="editing" ref="message" v-model="message" class="form-control mb-1"></textarea>
+      <button v-if="editing" v-on:click="createList" class="btn btn-secondary">Add</button>
+      <a v-if="editing" v-on:click="editing=false">Cancel</a>
+    </div>
   </draggable>
 </template>
 
@@ -9,13 +16,29 @@
   import list from 'components/list'
   export default {
     components: { draggable, list },
-    props: ["original_lists"],
     data: function() {
       return {
-        lists: this.original_lists,
+        editing: false,
+        message: "",
       }
     },
+
+    computed: {
+      lists: {
+        get() {
+          return this.$store.state.lists
+        },
+        set(value) {
+          this.$store.state.lists = value
+        },
+      },
+    },
+
     methods: {
+      startEditing: function() {
+        this.editing = true
+        this.$nextTick(() => { this.$refs.message.focus() })
+      },
       listMoved: function(event) {
         var data = new FormData
         data.append("list[position]", event.newIndex + 1)
@@ -26,6 +49,21 @@
           dataType: "json",
         })
       },
+      createList: function() {
+        var data = new FormData
+        data.append("list[name]", this.message)
+        Rails.ajax({
+          url: "/lists",
+          type: "POST",
+          data: data,
+          dataType: "json",
+          success: (data) => {
+            this.$store.commit('addList', data)
+            this.message = ""
+            this.editing = false
+          }
+        })
+      }
     }
   }
 </script>
@@ -37,5 +75,14 @@
   .board {
     overflow-x: auto;
     white-space: nowrap;
+  }
+  .list {
+    background: #E2E4E6;
+    border-radius: 3px;
+    display: inline-block;
+    margin-right: 20px;
+    padding: 10px;
+    vertical-align: top;
+    width: 270px;
   }
 </style>
--- app/javascript/components/list.vue
+++ app/javascript/components/list.vue
@@ -3,22 +3,21 @@
         <h6>{{ list.name }}</h6>
 
         <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
-            <div v-for="(card, index) in list.cards" class="card card-body mb-3">
-                {{ card.name }}
-            </div>
+            <card v-for="card in list.cards" :key="card.id" :card="card" :list="list"></card>
         </draggable>
 
         <a v-if="!editing" v-on:click="startEditing">Add a card</a>
         <textarea v-if="editing" ref="message" v-model="message" class="form-control mb-1"></textarea>
-        <button v-if="editing" v-on:click="submitMessage" class="btn btn-secondary">Add</button>
+        <button v-if="editing" v-on:click="createCard" class="btn btn-secondary">Add</button>
         <a v-if="editing" v-on:click="editing=false">Cancel</a>
     </div>
 </template>
 
 <script>
     import draggable from 'vuedraggable'
+    import card from 'components/card'
     export default {
-        components: { draggable },
+        components: { card, draggable },
         props: ["list"],
         data: function() {
             return {
@@ -35,13 +34,13 @@
                 const evt = event.added || event.moved
                 if (evt == undefined) { return }
                 const element = evt.element
-                const list_index = window.store.lists.findIndex((list) => {
+                const list_index = window.store.state.lists.findIndex((list) => {
                     return list.cards.find((card) => {
                         return card.id === element.id
                     })
                 })
                 var data = new FormData
-                data.append("card[list_id]", window.store.lists[list_index].id)
+                data.append("card[list_id]", window.store.state.lists[list_index].id)
                 data.append("card[position]", evt.newIndex + 1)
                 Rails.ajax({
                     url: `/cards/${element.id}/move`,
@@ -50,7 +49,7 @@
                     dataType: "json"
                 })
             },
-            submitMessage: function() {
+            createCard: function() {
                 var data = new FormData
                 data.append("card[list_id]", this.list.id)
                 data.append("card[name]", this.message)
@@ -60,8 +59,7 @@
                     data: data,
                     dataType: "json",
                     success: (data) => {
-                        const index = window.store.lists.findIndex(item => item.id == this.list.id)
-                        window.store.lists[index].cards.push(data)
+                        this.$store.commit('addCard', data)
                         this.message = ""
                         this.$nextTick(() => { this.$refs.message.focus() })
                     }
@@ -74,14 +72,5 @@
 <style scoped>
     .dragArea {
         min-height: 10px;
-    }
-    .list {
-        background: #E2E4E6;
-        border-radius: 3px;
-        display: inline-block;
-        margin-right: 20px;
-        padding: 10px;
-        vertical-align: top;
-        width: 270px;
     }
 </style>
